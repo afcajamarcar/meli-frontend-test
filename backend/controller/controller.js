67a114834cdfdb2f581adae8b5a58c5ecbe83dfd
Andres <afcajamarcar@unal.edu.co>
2020-09-08 16:00:06 -0500

    added services layer to remove responsibilities from controller

--- backend/controller/controller.js
+++ backend/controller/controller.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import { formatSearchResult } from '../services/formatterService';
+
 export const handleSearchAction = (req, res) => {
     const query = req.query.q;
     if (query) {
@@ -11,34 +13,7 @@ export const handleSearchAction = (req, res) => {
               const response = 
                 await axios.get(`http://api.mercadolibre.com/sites/MLA/search?q=${encodeURIComponent(query)}`);
               
-              const categoryFilter = response.data.available_filters.find(filter => filter.id == 'category') || 
-              response.data.filters.find(filter => filter.id == 'category');
-              const categoriesNames = categoryFilter && categoryFilter.values.map(value => value.name);
-              const filteredItemsByCategory = categoryFilter.values[0] && 
-                response.data.results.filter(item => item.category_id == categoryFilter.values[0].id);
-              const items = filteredItemsByCategory.map(item => {
-                return {
-                  id: item.id,
-                  title: item.title,
-                  price: {
-                    currency: item.currency_id,
-                    amount: item.available_quantity,
-                    decimals: item.price
-                  },
-                  picture: item.thumbnail,
-                  condition: item.condition,
-                  free_shipping: item.shipping.free_shipping,
-                  category: item.category_id
-                }
-              });
-              const formattedRes = {
-                author: {
-                  name: process.env.AUTHOR_NAME || 'Andres',
-                  lastname: process.env.AUTHOR_LASTNAME || 'Cajamarca'
-                },
-                categories: categoriesNames,
-                items: items.slice(0,4)
-              }
+              const formattedRes = formatSearchResult(response.data);
               return res.status(200).json(formattedRes);
             } catch (error) {
               console.log(error);
---
+++ backend/services/formatterService.js
@@ -0,0 +1,31 @@
+export const formatSearchResult = (data) => {
+    const categoryFilter = data.available_filters.find(filter => filter.id == 'category') ||
+        data.filters.find(filter => filter.id == 'category');
+    const categoriesNames = categoryFilter && categoryFilter.values.map(value => value.name);
+    const filteredItemsByCategory = categoryFilter.values[0] &&
+        data.results.filter(item => item.category_id == categoryFilter.values[0].id);
+    const items = filteredItemsByCategory.map(item => {
+        return {
+            id: item.id,
+            title: item.title,
+            price: {
+                currency: item.currency_id,
+                amount: item.available_quantity,
+                decimals: item.price
+            },
+            picture: item.thumbnail,
+            condition: item.condition,
+            free_shipping: item.shipping.free_shipping,
+            category: item.category_id
+        }
+    });
+    const formattedRes = {
+        author: {
+            name: process.env.AUTHOR_NAME || 'Andres',
+            lastname: process.env.AUTHOR_LASTNAME || 'Cajamarca'
+        },
+        categories: categoriesNames,
+        items: items.slice(0, 4)
+    }
+    return formattedRes;
+}
