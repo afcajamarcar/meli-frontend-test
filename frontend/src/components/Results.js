6ba0e057ddfc76691c8a0f64ad11dc560c165931
Andres <afcajamarcar@unal.edu.co>
2020-09-09 21:18:26 -0500

    styles for results view

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -1,11 +1,11 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import useSearchItems from '../../src/api/useSearchItems';
 
 import iconShipping from '../assets/images/ic_shipping.png';
 
 function useQuery() {
-	return new URLSearchParams(useLocation().search);
+    return new URLSearchParams(useLocation().search);
 }
 
 function Results() {
@@ -16,11 +16,11 @@ function Results() {
 
     const searchItems = useSearchItems(searchQuery);
 
-	useEffect(() => {
-		if (searchItems.response) {
-			setResults(searchItems.response);
-		}
-	}, [searchItems.response]);
+    useEffect(() => {
+        if (searchItems.response) {
+            setResults(searchItems.response);
+        }
+    }, [searchItems.response]);
 
     const formatPrice = price => {
         return new Intl.NumberFormat("es-ES").format(price);
@@ -28,22 +28,39 @@ function Results() {
 
     return (
         <div>
-            {results && results.items && results.items.map(item => (
-                <div key={item.id} style={{backgroundColor: 'white', borderBottom: '1px solid'}}>
-                    <img style={{width: '180px', height:'180px', borderRadius: '4px', padding: '16px'}} src={item.picture} alt={item.title}/>
-                    <div>
-                        <div style={{fontSize: '24px'}}>
-                            {`$ ${formatPrice(item.price.decimals)}`}
+            {results && results.items && results.items.length
+                ?
+                <div className='results-container'>
+                    {results && results.categories &&
+                        <div className='categories-container'>
+                            {results && results.categories &&
+                                results.categories.join(' > ')
+                            }
                         </div>
-                        {item.free_shipping &&
-                            <img src={iconShipping} alt="free_shipping"/>
-                        }
-                    </div>
-                    <div style={{fontSize: '18px'}}>
-                        {item.title}
-                    </div>
+                    }
+                    {results && results.items && results.items.map(item => (
+                        <div key={item.id} className='item-card'>
+                            <div className='left-side-card'>
+                                <img src={item.picture} alt={item.title} />
+                            </div>
+                            <div className='right-side-card'>
+                                <div className='price-container'>
+                                    {`$ ${formatPrice(item.price.decimals)}`}
+                                </div>
+                                {item.free_shipping &&
+                                    <img src={iconShipping} alt="free_shipping" title="Envío gratis" />
+                                }
+                                <div className='title-container'>
+                                    {item.title}
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-            ))}
+                :
+                <div></div>
+            }
+
         </div>
     );
 }
